d3d2e51a92f94278277c426fa1105a482811a4e2
Kunal Singh <kunalsin9h@gmail.com>
2021-11-28 12:20:46 -0800

    Add Bubble Sort

---
+++ Bubble Sort/BubbleSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+// Utility Function For Swaping Two Numbers
+void swap(int *a, int *b){
+    int c = *b;
+    *b = *a;
+    *a = c;
+}
+
+// Algo-> Bubble Sort
+// Time -> O(n^2)
+// Space -> O(1):
+// Inplace -> Yes
+// Stable -> Yes
+void BubbleSort(int arr[], int n){
+    bool isSorted = false;
+    int lastPass = n-1;
+    while(!isSorted){
+        isSorted = true;
+        for(int i = 0;i<lastPass; ++i){
+            if(arr[i] > arr[i+1]){
+                swap(&arr[i], &arr[i+1]);
+                isSorted = false;      
+            }
+        }
+        lastPass--; // Little Bit optimization
+    }
+}
+
+int main(){
+    int arr[] = {13, 3, 3544, 45, 32, 3, 2, 1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    BubbleSort(arr, n);
+    for(int i = 0;i<n; ++i){
+        cout << arr[i] << " ";
+    }   
+    cout << endl;
+    return 0;
+}
---
+++ Bubble Sort/experiment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+
+int main(){
+    bool ch = true;
+    while(ch){
+        ch = false;
+        cout << "Chal Gaya Maza AA Gaya!" << endl;
+        ch = true;
+    }
+    cout << ch << endl;   
+    return 0;
+}
