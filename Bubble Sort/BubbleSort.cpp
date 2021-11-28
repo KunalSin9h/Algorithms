a5c7c9ba32943735d18b8b730bf44f0cf9bc458d
Kunal Singh <kunalsin9h@gmail.com>
2021-11-28 12:21:40 -0800

    Update BubbleSort.cpp

--- Bubble Sort/BubbleSort.cpp
+++ Bubble Sort/BubbleSort.cpp
@@ -30,6 +30,22 @@ void BubbleSort(int arr[], int n){
     }
 }
 
+void Bubble2(int arr[], int n){
+    int counter = 1;
+    while(counter < n){
+        for(int i = 0;i<n-counter; ++i){
+            if(arr[i] > arr[i+1]){
+                int temp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        counter++;
+    }
+
+
+}
+
 int main(){
     int arr[] = {13, 3, 3544, 45, 32, 3, 2, 1};
     int n = sizeof(arr)/sizeof(arr[0]);
