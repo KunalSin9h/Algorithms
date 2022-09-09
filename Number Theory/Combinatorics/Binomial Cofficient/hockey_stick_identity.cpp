20feffef2e98c158ebc0ed8cb88668dfa7f3e5a2
Kunal Singh <kunalsin9h@gmail.com>
2022-09-09 16:52:41 +0530

    algo

---
+++ Number Theory/Combinatorics/Binomial Cofficient/hockey_stick_identity.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/**
+ *Question : https://codeforces.com/contest/660/problem/E 
+ **/
+
+/** Hockey Stick Identity
+ *  Blog : https://codeforces.com/blog/entry/104172
+ *
+ *  For whole number n and r (n >= r),
+ *
+ *  S[k = {r, n}] kCr = n+1Cr+1. 
+ *
+ *  i.e 
+ *  n = 7
+ *  r = 3
+ *
+ *  so...
+ *  3C3 + 4C3 + 5C3 + 6C3 + 7C3 = 8C4
+ **/
+
+int main(){
+    return 0;
+}
